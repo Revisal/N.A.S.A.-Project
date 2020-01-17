33e9bcd66f7e30b92b5609b3a160b21caf7ff785
JadeKirby <54641594+JadeKirby@users.noreply.github.com>
2020-01-17 13:01:20 -0500

    Add files via upload
    Friction calculation.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace NASAFormApplication
+namespace Friction
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            // Variables needed for friction 
+            const double MOON_MASS = 7.34767309E+22;
+            const double MOON_GRAV_ACC = 1.62;
+            double degree = 0;
 
+            // Allows the user to enter an angle in that is less than 10
+            Console.Write("Enter the angle of launch(Must be less than 10 degrees): ");
+            degree = Convert.ToDouble(Console.ReadLine());
 
-         
+            while (degree > 10)
+            {
+                Console.Write("Invalid input, try again: ");
+                degree = Convert.ToDouble(Console.ReadLine());
+            }
+
+            // Calculates normal force 
+            double normalForce = MOON_MASS * MOON_GRAV_ACC;
+
+            // Converts from radians to degrees
+            double newAngle = (degree * (Math.PI)) / 180;
+
+            // Calculates tangential force
+            double tangentialForce = normalForce * Math.Sin(newAngle);
+
+            // Calculates friction
+            double friction = tangentialForce / normalForce;
+
+            // Displays friction
+            Console.Write("Friction on the moon is: " + friction);
         }
     }
 }
